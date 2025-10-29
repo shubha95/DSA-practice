aeb6f8656bdf7f581e705357d55741de843645eb
shubha95 <shubha95@example.com>
2025-10-30 00:04:19 +0530

    insert and traves Linked List

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,11 @@ import { LinkedList } from './linkedlist';
 
 function demo() {
   const list = new LinkedList<number>();
-  console.log('LinkedList instance created:', list.createLinkedList());
-//   list.push(10);
-//   list.push(20);
-//   list.insertAt(1, 15);
-
-//   console.log('List contents:', list.toArray());
-//   console.log('Removed at 1:', list.removeAt(1));
-//   console.log('After remove:', list.toArray());
+  list.createLinkedList()
+  list.insertLinkedList(10)
+  list.insertLinkedList(20)
+  list.travesLinkedList();
+  console.log("Demo complete");
 }
 
 demo();
--- src/linkedlist.ts
+++ src/linkedlist.ts
@@ -1,12 +1,43 @@
 import { Node } from './node';
 
 export class LinkedList<T> {
-  head: Node<T> | null = null;
-  size = 0;
-  tail: Node<T> | null = null;
+  private head: Node<T> | null = null; 
+  private tail: Node<T> | null = null;
+  private size: number = 0;
 
-  createLinkedList(): LinkedList<T>{
-    return new LinkedList<T>();
+  createLinkedList(): LinkedList<T> {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+    return this;
   }
+ 
+insertLinkedList(value: T): LinkedList<T> {
+  const tempNode = new Node<T>(value);
+  if(this.head === null){
+    this.head = this.tail = tempNode;
+  } else {
+    if (this.tail) {
+      this.tail.next = tempNode;
+      this.tail = tempNode;
+    }
+  }
+    this.size++
+  return this;
+}
+ 
 
+travesLinkedList(): LinkedList<T> {
+  let current = this.head;
+  if (!current) {
+    console.log('LinkedList is empty');
+    return this;
+  }
+  while (current) {
+    process.stdout.write(`${current.value} ->`);
+    current = current.next;
+  }
+  console.log('');
+  return this;
+}
 }
